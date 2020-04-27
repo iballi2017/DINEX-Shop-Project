d9cd07f20a6914255dfc53e57496298c99e0edf8
iballi2017 <iballi2017@gmail.com>
2020-04-27 07:41:24 +0100

    27/04/2020 commit

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ $(document).ready(() => {
         if (x == true) {
             $(".nav-tab").show(1000);
             x = false
-            console.log("x = false");
             $(".fa-ellipsis-v").css({ "transform": "rotate(90deg)", "transition": "transform 250ms" })
             $(".menu-btn").css({ "color": "#858585" })
         }
